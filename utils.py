4535edbd9d480f74293348f6fe46b3e50161a1b7
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-09-06 22:40:45 +0800

    update

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import collections
+import pickle
 import pandas as pd
 
 
@@ -39,9 +40,21 @@ def read_csv(
 
 class Convert:
 
-    def __init__(self, lst: list):
+    def __init__(self, lst: list, file_path='./item_list.pkl'):
         self.lst = lst
-        self.item_list, self.item_dict = self.parse()
+        self.file_path = file_path
+
+        # 对 lst 进行编码
+        self.item_list = None
+        self.item_dict = None
+        self.parse()
+
+    @staticmethod
+    def to_dict(item_list):
+        item_dict = dict()
+        for i, item in enumerate(item_list):
+            item_dict[item] = i
+        return item_dict
 
     def parse(self):
         """对列表元素编码"""
@@ -53,12 +66,13 @@ class Convert:
         sorted_items = sorted(frequency.items(),
                               key=lambda e: e[1],
                               reverse=True)
-        item_list = [e[0] for e in sorted_items]
-        item_dict = dict()
-        for i, item  in enumerate(item_list):
-            item_dict[item] = i
 
-        return item_list, item_dict
+        self.item_list = [e[0] for e in sorted_items]
+        self.item_dict = self.to_dict(self.item_list)
+
+    def reset(self):
+        self.item_list = self.read()
+        self.item_dict = self.to_dict(self.item_list)
 
     def __len__(self):
         return len(self.item_list)
@@ -66,5 +80,14 @@ class Convert:
     def __getitem__(self, index):
         return self.item_list[index]
 
-    def encoder(self, item ):
+    def encoder(self, item):
         return self.item_dict.get(item)
+
+    def save(self):
+        with open(self.file_path, 'wb') as f:
+            pickle.dump(self.item_list, f)
+
+    def read(self):
+        with open(self.file_path, 'rb') as f:
+            item_list = pickle.load(f)
+        return item_list
